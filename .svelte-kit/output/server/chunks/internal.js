8592f39bb12866acf7126bb0dee78d3de7bff19e
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2025-10-04 11:10:34 -0700

    fix for vercel
    fix for vercel

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -2505,7 +2505,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1kqb3j3"
+  version_hash: "is5f9b"
 };
 async function get_hooks() {
   let handle;
